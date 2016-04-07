8b41b8d3f64cc1a92ec8ad2d27df96e9650f6847
Sysop <sysop@localhost.localdomain>
2016-04-07 15:15:11 +0200

    update directive template

--- directive/templates/js/directive.js
+++ directive/templates/js/directive.js
@@ -1,19 +1,19 @@
 (function () {
-  'use strict';
-  /**
-   * @ngdoc directive
-   * @name <%= scriptAppName %>.directive:<%= scriptClassName %>
-   * @description
-   * # <%= scriptClassName %>
-   */
-  angular.module('<%= scriptAppName %>')
-    .directive('<%= scriptClassName %>', function () {
-      return {
-        template: '<div></div>',
-        restrict: 'E',
-        link: function (scope, element, attrs) {
-          element.text('this is the <%= scriptClassName %> directive');
-        }
-      };
-    })
+    'use strict';
+    /**
+     * @ngdoc directive
+     * @name quizAppAdmin.directive:test
+     * @description
+     * # test
+     */
+    angular.module('<%= scriptAppName %>')
+        .directive('<%= scriptClassName %>', function () {
+            return {
+                template: '<div></div>',
+                restrict: 'E',
+                link: function (scope, element, attrs) {
+                    element.text('this is the <%= scriptClassName %> directive');
+                }
+            };
+        });
 })();
