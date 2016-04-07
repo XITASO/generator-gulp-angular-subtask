7e7e3076b2135edb1978a6a7f5124e06b75eed0c
Sysop <sysop@localhost.localdomain>
2016-04-07 15:12:06 +0200

    xitaso eslint

--- directive/templates/js/spec/directive.js
+++ directive/templates/js/spec/directive.js
@@ -1,27 +1,27 @@
 'use strict';
 
-describe('Directive: <%= scriptClassName %>', function () {
-  // load the directive's module
-  beforeEach(module('<%= scriptAppName %>'));
-  var $compile,
-    $scope,
-    directiveElem;
+describe('Directive: test', function () {
+    // load the directive's module
+    beforeEach(module('quizAppAdmin'));
+    var $compile,
+        $scope,
+        directiveElem;
 
-  beforeEach(inject(function (_$compile_, _$rootScope_) {
-    $compile = _$compile_;
-    $scope = _$rootScope_.$new();
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $scope = _$rootScope_.$new();
 
-    directiveElem = getCompiledElement();
-  }));
+        directiveElem = getCompiledElement();
+    }));
 
-  function getCompiledElement() {
-    var element = angular.element(/*directive html tag*/);
-    var compiledElement = $compile(element)($scope);
-    $scope.$digest();
-    return compiledElement;
-  }
+    function getCompiledElement() {
+        var element = angular.element(/*directive html tag*/);
+        var compiledElement = $compile(element)($scope);
+        $scope.$digest();
+        return compiledElement;
+    }
 
-  it('should load/show <%= scriptClassName %> html', inject(function () {
-    expect(directiveElem.text()).not.toEqual('');
-  }));
+    it('should load/show test html', inject(function () {
+        expect(directiveElem.text()).not.toEqual('');
+    }));
 });
