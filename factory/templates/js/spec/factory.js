fcde9e6519d96eb978c9646e666f0b33a9366ab0
Sysop <sysop@localhost.localdomain>
2016-04-07 17:05:59 +0200

    fix typo

--- factory/templates/js/spec/factory.js
+++ factory/templates/js/spec/factory.js
@@ -2,7 +2,7 @@ describe('Factory: <%= scriptClassName %>Factory', function () {
     var <%= scriptClassName %>;
     beforeEach(module('<%= scriptAppName %>'));
     beforeEach(inject(function (_<%= scriptClassName %>_) {
-        test = _<%= scriptClassName %>;_;
+        test = _<%= scriptClassName %>_;
     }));
 
     it('should provide the meaning of life', function () {
